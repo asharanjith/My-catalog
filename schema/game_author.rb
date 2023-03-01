64af95e9a69523154d92debda90de41238c3a420
bornittah <bornittah@gmail.com>
2023-03-01 17:56:37 +0300

    Add foreign key

--- schema/game_author.rb
+++ schema/game_author.rb
@@ -1,14 +1,14 @@
-CREATE TABLE IF NOT EXISTS 'authors' (
+CREATE TABLE IF NOT EXISTS 'author' (
   'id' INTEGER PRIMARY KEY AUTOINCREMENT,
   'first_name' TEXT NOT NULL,
   'last_name' TEXT NOT NULL,
   'publish_date' DATE NOT NULL,
-  'archived' BOOLEAN NOT NULL DEFAULT FALSE,
+  'item_id' INTEGER REFERENCES 'item' ('id')
 );
 
 CREATE TABLE IF NOT EXISTS 'game' (
   'id' INTEGER PRIMARY KEY AUTOINCREMENT,
   'multiplayer' TEXT NOT NULL,
   'last_played_at' DATE NOT NULL,
-  'archived' BOOLEAN NOT NULL DEFAULT FALSE,
+  'item_id' INTEGER REFERENCES 'item' ('id')
 );
