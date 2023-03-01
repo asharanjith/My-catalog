79d6b5f9125f76476dc9703cf78e6002ce0332bb
bornittah <bornittah@gmail.com>
2023-03-01 18:01:21 +0300

    Fix linters errors

--- schema/game_author.rb
+++ schema/game_author.rb
@@ -1,14 +1,14 @@
 CREATE TABLE IF NOT EXISTS 'author' (
-  'id' INTEGER PRIMARY KEY AUTOINCREMENT,
-  'first_name' TEXT NOT NULL,
-  'last_name' TEXT NOT NULL,
-  'publish_date' DATE NOT NULL,
-  'item_id' INTEGER REFERENCES 'item' ('id')
+  'id' integer PRIMARY KEY AUTOINCREMENT,
+  'first_name' text NOT NULL,
+  'last_name' text NOT NULL,
+  'publish_date' date NOT NULL,
+  'item_id' integer REFERENCES 'item'
 );
 
 CREATE TABLE IF NOT EXISTS 'game' (
-  'id' INTEGER PRIMARY KEY AUTOINCREMENT,
-  'multiplayer' TEXT NOT NULL,
-  'last_played_at' DATE NOT NULL,
-  'item_id' INTEGER REFERENCES 'item' ('id')
+  'id' integer PRIMARY KEY AUTOINCREMENT,
+  'multiplayer' text NOT NULL,
+  'last_played_at' date NOT NULL,
+  'item_id' integer REFERENCES 'item'
 );
