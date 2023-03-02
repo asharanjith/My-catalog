1563ff18a2b72f4df5b2a15bfb02d5eaa6e80133
Tamunokuro <tamunokrobluejack@gmail.com>
2023-03-02 14:14:34 +0100

    Add schema for MusicAlbum and Genre

--- schema/schema.sql
+++ schema/schema.sql
@@ -10,3 +10,24 @@ CREATE TABLE IF NOT EXISTS 'game' (
   'last_played_at' date NOT NULL,
   'item_id' integer REFERENCES 'item'
 );
+
+CREATE TABLE Genre (
+  id SERIAL NOT NULL,
+  name VARCHAR(50) NOT NULL,
+  PRIMARY KEY (id)
+)
+
+CREATE MusicAlbum (
+  id SERIAL NOT NULL,
+  on_spotify BOOLEAN NOT NULL,
+  publish_date DATE NOT NULL, 
+  archived BOOLEAN NOT NULL, 
+  genre_id INT NULL,
+  author_id INT NULL,
+  label_id INT NULL,
+  FOREIGN KEY (genre_id) REFERENCES Genre (id),
+  FOREIGN KEY (author_id) REFERENCES Author (id),
+  FOREIGN KEY (label_id) REFERENCES Label (id),
+  PRIMARY KEY (id)
+)
+
